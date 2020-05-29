c4d343210d495b69218049b4c177a594acfb88e5
Benjamin Duffield <bduffield@palantir.com>
2020-05-28 21:51:43 -0400

    make it more of a cli

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use core_foundation::base::*;
 use core_foundation::number::*;
 use core_foundation::string::*;
 use core_graphics::display::*;
-use std::ffi::{c_void, CStr};
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{ErrorKind, Write};
+use std::path::PathBuf;
 use std::{thread, time};
+use structopt::StructOpt;
 
 unsafe fn get_window_layer(window_info: CFDictionaryRef) -> Option<i32> {
     let window_info = CFDictionary::<CFString, CFNumber>::wrap_under_get_rule(window_info);
@@ -55,19 +58,43 @@ unsafe fn get_active_window_title() -> Option<String> {
     None
 }
 
-fn main() -> Result<(), std::io::Error> {
+#[derive(StructOpt)]
+struct Cli {
+    /// The directory to output log files. Defaults to current directory.
+    #[structopt(parse(from_os_str))]
+    path: Option<PathBuf>,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let args: Cli = Cli::from_args();
+
+    let output_directory = args.path.unwrap_or(PathBuf::from("."));
+    if !output_directory.is_dir() {
+        return Err(std::io::Error::from(ErrorKind::InvalidInput)).with_context(|_| {
+            format!(
+                "Output directory was not a directory: {:?}",
+                output_directory
+            )
+        })?;
+    }
+
     let log_file_path = Utc::now().date().format("%Y-%m-%d.log.tsv");
     let mut log_file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(log_file_path.to_string())?;
+        .open(log_file_path.to_string())
+        .with_context(|_| format!("Unable to open output file: {:}", log_file_path.to_string()))?;
+
     println!("Writing log to {}", log_file_path);
+
+    let bar = indicatif::ProgressBar::new_spinner();
+    bar.enable_steady_tick(100);
 
     loop {
         if let Some(active_window) = unsafe { get_active_window_title() } {
             let now = Utc::now().to_rfc3339();
 
-            println!("{}\t{}\n", now, active_window);
+            bar.set_message(active_window.as_str());
 
             log_file.write_fmt(format_args!("{}\t{}\n", now, active_window))?;
             log_file.flush()?;
